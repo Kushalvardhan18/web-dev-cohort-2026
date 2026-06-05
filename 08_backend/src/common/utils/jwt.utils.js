9b199d446cc54574e21dc9dafd86fd5d32c20bff
Kushal Vardhan <vardhank140102@gmail.com>
2026-06-05 14:26:50 +0530

    json web tokens

--- 08_backend/src/common/utils/jwt.utils.js
+++ 08_backend/src/common/utils/jwt.utils.js
@@ -1,4 +1,27 @@
 import crypto from "crypto"
+import jwt from "jsonwebtoken"
+
+const generateAccessToken = (payload) => {
+    return jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
+        expiresIn: process.env.JWT_ACCESS_EXPIRES_IN || '15m'
+    })
+
+}
+
+const verifyAccessToken = (token) => {
+    return jwt.verify(token, process.env.JWT_ACCESS_SECRET)
+}
+
+const generateRefreshToken = (payload) => {
+    return jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
+        expiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '7d'
+    })
+
+}
+
+const verifyRefreshToken = (token) => {
+    return jwt.verify(token, process.env.JWT_REFRESH_SECRET)
+}
 
 
 const generateResetToken = () => {
@@ -9,4 +32,4 @@ const generateResetToken = () => {
     return { rawToken, hashedToken }
 }
 
-export { generateResetToken }
+export { generateResetToken, generateAccessToken ,generateRefreshToken,verifyAccessToken,verifyRefreshToken}
--- 08_backend/src/modules/auth/auth.service.js
+++ 08_backend/src/modules/auth/auth.service.js
@@ -8,7 +8,7 @@ const register = async ({ name, email, password, role }) => {
 
     const { rawToken, hashedToken } = generateResetToken()
 
-   const user= await User.create({
+    const user = await User.create({
         name,
         email,
         password,
@@ -16,7 +16,7 @@ const register = async ({ name, email, password, role }) => {
     })
 
     console.log(user);
-    
+
     // const userObj = user.toObject()
     // delete userObj.password
     // delete userObj.verificationToken
