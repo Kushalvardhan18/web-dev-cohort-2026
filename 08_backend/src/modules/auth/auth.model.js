f678f171a069d45d4ce09393646ead40b1b4467c
Kushal Vardhan <vardhank140102@gmail.com>
2026-06-14 15:35:59 +0530

    mongo middleware, password hashing and mongo methods

--- 08_backend/src/modules/auth/auth.model.js
+++ 08_backend/src/modules/auth/auth.model.js
@@ -1,41 +1,54 @@
-import mongoose from "mongoose"
+import mongoose, { Schema } from "mongoose"
+import bcrypt from "bcryptjs"
 
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
         trim: true,
-        minlength:2,
-        maxlength:50,
-        required:[true,"Name is required"]
+        minlength: 2,
+        maxlength: 50,
+        required: [true, "Name is required"]
     },
     email: {
         type: String,
         trim: true,
-        required:[true,"Email is required"],
-        unique:true,
-        lowercase:true
+        required: [true, "Email is required"],
+        unique: true,
+        lowercase: true
     },
-    password:{
-        type:String,
-        minlength:8,
-        required:[true,"Password is required"],
-        select:false
+    password: {
+        type: String,
+        minlength: 8,
+        required: [true, "Password is required"],
+        select: false
     },
-    role:{
-        type:String,
-        enum:["user","admin"],
-        default:"user",
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user",
     },
-    isVerified:{
-        type:Boolean,
-        default:false,
+    isVerified: {
+        type: Boolean,
+        default: false,
     },
-    verificationToken:{type:String,select:false},
-    refreshToken:{type:String,select:false},
-    resetPasswordToken:{type:String,select:false},
-    resetPasswordExpires:{type:Date,select:false},
-    
-},{timestamps:true})
+    verificationToken: { type: String, select: false },
+    refreshToken: { type: String, select: false },
+    resetPasswordToken: { type: String, select: false },
+    resetPasswordExpires: { type: Date, select: false },
+
+}, { timestamps: true })
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified("password")) return next()
+
+    this.password = await bcrypt.hash(this.password, 12)
+    next()
+})
+
+
+userSchema.methods.comparePassword = async function (clearTextPassword) {
+   return  bcrypt.compare(clearTextPassword, this.password)
+}
 
 export default mongoose.model("User", userSchema)
 //users --- when goes in db it becomes plural and lowercase
--- 08_backend/src/modules/auth/auth.route.js
+++ 08_backend/src/modules/auth/auth.route.js
@@ -2,10 +2,15 @@ import { Router } from "express"
 import * as controller from "./auth.controller.js"
 import validate from "../../common/dto/base.dto.js"
 import RegisterDto from "./dto/register.dto.js"
+import { isLoggedIn } from "./auth.middleware.js"
+import LoginDto from "./dto/login.dto.js"
 
 const router = Router()
 
 router.post("/register", validate(RegisterDto), controller.register)
 
+router.post("/login", validate(LoginDto), controller.login)
+
+router.get("/me", isLoggedIn, controller.getMe)
 
 export default router
